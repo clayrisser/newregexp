11e26a34bb5187e6caa19b130ca8877b2643ce33
Jam Risser <jam@codejam.ninja>
2018-10-31 16:25:03 -0500

    Update babel to 7

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 export default function newRegExp(regexString) {
   let expression = regexString;
   let flags = '';
-  if (/^\/((\\\/)|[^\/])*\//.test(regexString)) {
-    expression = (regexString.match(/^\/((\\\/)|[^\/])*/g) || [])
+  if (/^\/((\\\/)|[^/])*\//.test(regexString)) {
+    expression = (regexString.match(/^\/((\\\/)|[^/])*/g) || [])
       .join('')
       .substr(1);
-    flags = (regexString.match(/[^\/]*$/g) || []).join('');
+    flags = (regexString.match(/[^/]*$/g) || []).join('');
   }
   return new RegExp(expression, flags);
 }
